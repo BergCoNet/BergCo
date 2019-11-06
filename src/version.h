c902af8d38b1062442ecb8acc8a1397693a45f5a
BergCoNet <55743338+BergCoNet@users.noreply.github.com>
2019-11-06 12:54:56 +0000

    Version Updated
    Version Upped to V1.1.0.0-Freeze

--- src/version.h
+++ src/version.h
@@ -35,17 +35,17 @@ static const int DATABASE_VERSION = 70509;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 70045;
+static const int PROTOCOL_VERSION = 70048;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70045;
+static const int MIN_PEER_PROTO_VERSION = 70048;
 
-static const int MIN_INSTANTX_PROTO_VERSION = 70045;
+static const int MIN_INSTANTX_PROTO_VERSION = 70048;
 
-static const int MIN_MN_PROTO_VERSION = 70045;
+static const int MIN_MN_PROTO_VERSION = 70048;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
@@ -53,7 +53,7 @@ static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 0;
-static const int NOBLKS_VERSION_END = 61960;
+static const int NOBLKS_VERSION_END = 70013;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60001;
